f84b639032ea8d460665485a558c4ca541cb5294
wish13t <vishal13joshi@gmail.com>
2020-02-06 14:30:06 +0530

    SignupConnection CSS styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/new--no-test-framework'
+  get 'employee/new--no-test-framework'
   get 'pardesi_jobs/home'
   get 'pardesi_jobs/services'
   get 'pardesi_jobs/contact'
